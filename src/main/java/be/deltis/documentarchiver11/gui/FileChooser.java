c8c24248b2348fd5a7e973ab3b14f7c5dbb01a38
Benoit Devos <benoit.devos@deltis.be>
2019-11-29 17:23:36 +0100

    Fixed FileChooser not being properly closed.

--- src/main/java/be/deltis/documentarchiver11/gui/FileChooser.java
+++ src/main/java/be/deltis/documentarchiver11/gui/FileChooser.java
@@ -3,35 +3,29 @@ package be.deltis.documentarchiver11.gui;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.File;
 
 @Component
-public class FileChooser extends JFileChooser {
+public class FileChooser extends JFrame {
+
+    private JFileChooser fileChooser;
 
     public FileChooser() {
-        super();
-        setDialogTitle("Directory selector");
-        setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        setAcceptAllFileFilterUsed(false);
+        fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Directory selector");
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        getContentPane().add(fileChooser);
     }
 
     public String selectDirectory(String currentDirectory) {
-        setCurrentDirectory(new File(currentDirectory));
-        JFrame frame = new JFrame();
-        frame.addWindowListener(
-                new WindowAdapter() {
-                    public void windowClosing(WindowEvent e) {
-                        frame.dispose();
-                    }
-                }
-        );
-        frame.getContentPane().add(this);
-        frame.setVisible(true);
-
-        if (showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-            return getSelectedFile().getAbsolutePath();
+        fileChooser.setCurrentDirectory(new File(currentDirectory));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        int option = fileChooser.showOpenDialog(this);
+        dispose();
+        if (option == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile().getAbsolutePath();
         } else {
             return null;
         }
